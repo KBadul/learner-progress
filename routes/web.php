ac4f41f4f1467641d0a9ef2ed1d0f1f051274b55
KeshavEmbryo <keshav.badul06@gmail.com>
2025-12-14 13:54:42 +0200

    Remove welcome view and update web routes
    Deleted the default welcome.blade.php view and made changes to routes/web.php, likely to remove or replace the default landing page. This prepares the application for a custom entry point or a new homepage.

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\LearnerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/learner-progress', [LearnerController::class, 'index'])->name('learners.index');
